a91ceef1ea9560dfa14699cd32e049aa9b0fd46a
Alexander Staubo <alex@bengler.no>
2009-05-04 13:14:19 +0200

    Retry queries on database disconnection.

--- lib/geo_service/database.rb
+++ lib/geo_service/database.rb
@@ -13,27 +13,72 @@ module GeoService
   class Database
     
     def initialize(options)
-      @connection = PGconn.open(
-        :host => options["host"],
-        :port => options["port"],
-        :dbname => options["name"],
-        :user => options["user"],
-        :password => options["password"])
+      @options = options
+      connect
     end
     
     def query(sql, &block)
-      entries = @connection.query(sql).entries
-      if block
-        return entries.each(&block)
+      try_database do
+        entries = @connection.query(sql).entries
+        if block
+          return entries.each(&block)
+        else
+          return entries
+        end
+      end        
+    end
+    
+    def connect
+      @connection = PGconn.open(
+        :host => @options["host"],
+        :port => @options["port"],
+        :dbname => @options["name"],
+        :user => @options["user"],
+        :password => @options["password"])
+    end
+    
+    # Close the connection.
+    def disconnect!
+      @connection.close rescue nil
+    end
+
+    # Is this connection alive and ready for queries?
+    def active?
+      if @connection.respond_to?(:status)
+        return @connection.status == PGconn::CONNECTION_OK
       else
-        return entries
+        begin
+          @connection.query('select 1')
+        rescue
+          return false
+        else
+          return true
+        end
       end
     end
-    
-    def close
-      @connection.finish
+
+    # Close then reopen the connection.
+    def reconnect!
+      if @connection.respond_to?(:reset)
+        @connection.reset
+      else
+        disconnect!
+        connect
+      end
     end
+
+    protected
     
+      def try_database(&block)
+        begin
+          return yield
+        rescue PGError => e
+          raise e if active?
+          reconnect!
+          return yield
+        end
+      end
+
   end
   
 end
